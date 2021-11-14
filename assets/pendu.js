37cccce0353d2fe3f284ee82801e718092e837c6
Angeline <angelinecoignet@gmail.com>
2021-11-14 14:36:45 +0100

    Ajout du pendu dessiné pas à pas à chaque erreur

--- assets/pendu.js
+++ assets/pendu.js
@@ -4,6 +4,53 @@ let wordToFind = '';
 let lifes = 8;
 
 
+let myCanvas = document.getElementById('test')
+let graph = myCanvas.getContext('2d');
+
+//Dessiner une partie du pendu à chaque erreur
+function drawHanged(lifes) {
+    graph.beginPath();
+    switch (lifes) {
+        case 7:
+            graph.moveTo(50, 125);
+            graph.lineTo(100, 125);
+            break;
+        case 6:
+            graph.moveTo(75, 125);
+            graph.lineTo(75, 25);
+            break;
+        case 5:
+            graph.moveTo(75, 25);
+            graph.lineTo(150, 25);
+            break;
+        case 4:
+            graph.moveTo(150, 25);
+            graph.lineTo(150, 37);
+            break;
+        case 3:
+            graph.moveTo(162, 50);
+            graph.arc(150, 50, 13, 0, 2 * Math.PI);
+            break;
+        case 2:
+            graph.moveTo(150, 62);
+            graph.lineTo(150, 87);
+            break;
+        case 1:
+            graph.moveTo(130, 75);
+            graph.lineTo(170, 75);
+            break;
+        case 0:
+            graph.moveTo(162, 100);
+            graph.lineTo(150, 87);
+            graph.lineTo(137, 100);
+            break;
+    }
+    graph.stroke();
+}
+
+
+
+
 //Afficher un mot au hasard du tableau animals
 function pickWord(list) {
     return list[Math.floor(Math.random() * list.length)];
@@ -32,7 +79,7 @@ function resetKeyboard() {
     document.addEventListener('click', chooseLetter);
     let elts = document.getElementsByTagName('button')
 
-    for(i=0; i<elts.length; i++){
+    for (i = 0; i < elts.length; i++) {
         elts[i].disabled = false;
         elts[i].style.backgroundColor = '#7DAA92';
     }
@@ -50,6 +97,7 @@ function newPlay(word) {
     wordToFind = word.toUpperCase();
     resetKeyboard();
     document.getElementById('endPlay').textContent = '';
+    graph.clearRect(0, 0, myCanvas.width, myCanvas.height);
 }
 
 document.getElementById('newWord').onclick = () => (newPlay(pickWord(animals)));
@@ -62,23 +110,24 @@ function chooseLetter(element) {
         let letterClick = element.target.textContent;
         let answerWord = searchIndex(wordToFind, letterClick);
 
-        if(answerWord.length != 0){
+        if (answerWord.length != 0) {
             document.getElementById('answer').textContent = replaceLetters(letterClick, answerWord);
-            if(!wordString.includes('_') && lifes > 0){
+            if (!wordString.includes('_') && lifes > 0) {
                 document.getElementById('endPlay').textContent = `VICTOIRE !`;
                 document.removeEventListener('click', chooseLetter); //Désactiver clavier en cas de victoire
             }
 
-        }else{
-           lifes--;
-           if(lifes <= 0){
-            lifes = 0;
-            document.getElementById('endPlay').textContent = 'PERDU !'
-           }
-           document.getElementById('lifeLeft').textContent = lifes; 
+        } else {
+            lifes--;
+            drawHanged(lifes)
+            if (lifes <= 0) {
+                lifes = 0;
+                document.getElementById('endPlay').textContent = 'PERDU !'
+            }
+            document.getElementById('lifeLeft').textContent = lifes;
         }
 
-        
+
         console.log(answerWord);
         element.target.style.backgroundColor = '#D1C8E1';
         element.target.disabled = true;
