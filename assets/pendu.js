fe6edeecd00aba7eb3142e823df4814b8a6082ea
Angeline <angelinecoignet@gmail.com>
2021-11-14 12:22:33 +0100

    Correctif des bugs + décompte des vies + réinitialisation partie

--- assets/pendu.js
+++ assets/pendu.js
@@ -1,6 +1,8 @@
 let animals = ['vache', 'chien', 'poisson', 'lion', 'koala'];
 let wordString = '';
 let wordToFind = '';
+let lifes = 8;
+
 
 //Afficher un mot au hasard du tableau animals
 function pickWord(list) {
@@ -17,49 +19,66 @@ function searchIndex(word, letter) {
             arrayIndex.push(index);
         }
     }
-
-    if (arrayIndex.length === 0) {
-        return -1;
-    } else {
-        return arrayIndex;
-    }
+    return arrayIndex;
+    // if (arrayIndex.length === 0) {
+    //     return -1;
+    // } else {
+    //     return arrayIndex;
+    // }
 }
 
 //Afficher des underscores à la place du mot selon son nombre de caractères et réinitialiser clavier
 function resetKeyboard() {
+    document.addEventListener('click', chooseLetter);
+    let elts = document.getElementsByTagName('button')
 
+    for(i=0; i<elts.length; i++){
+        elts[i].disabled = false;
+        elts[i].style.backgroundColor = '#7DAA92';
+    }
 }
 
-function displayUnderscore(word) {
+function newPlay(word) {
     let wordChoosen = word.split('');
     wordString = '';
+    lifes = 8;
+    document.getElementById('lifeLeft').textContent = lifes;
     for (i = 0; i < wordChoosen.length; i++) {
         wordString += '_';
     }
     document.getElementById('answer').innerHTML = wordString;
     wordToFind = word.toUpperCase();
+    resetKeyboard();
+    document.getElementById('endPlay').textContent = '';
 }
 
-document.getElementById('newWord').onclick = () => (displayUnderscore(pickWord(animals)));
+document.getElementById('newWord').onclick = () => (newPlay(pickWord(animals)));
 
-//Afficher la lettre correspondant au bouton cliqué et le désactiver
-// function chooseLetter(element){
-//     if(element.target.nodeName == 'BUTTON'){
-//         if(element.target.id != 'newWord'){
-//            let idButton = element.target.id
-//            document.getElementById('letterChoosen').innerHTML += idButton.toUpperCase()
-//            document.getElementById(idButton).style.backgroundColor = '#D1C8E1' 
-//            document.getElementById(idButton).disabled = true
-//         }        
-//     }    
-// }
 
-//Ajoute la lettre au mot cherché et désactive la lettre cliquée
+//Ajouter la lettre au mot cherché, désactiver la lettre cliquée, retirer une vie pour chaque erreur
 function chooseLetter(element) {
+
     if (element.target.nodeName == 'BUTTON' && element.target.id != 'newWord') {
         let letterClick = element.target.textContent;
         let answerWord = searchIndex(wordToFind, letterClick);
-        document.getElementById('answer').textContent = replaceLetters(letterClick, answerWord);
+
+        if(answerWord.length != 0){
+            document.getElementById('answer').textContent = replaceLetters(letterClick, answerWord);
+            if(!wordString.includes('_') && lifes > 0){
+                document.getElementById('endPlay').textContent = `VICTOIRE !`;
+                document.removeEventListener('click', chooseLetter); //Désactiver clavier en cas de victoire
+            }
+
+        }else{
+           lifes--;
+           if(lifes <= 0){
+            lifes = 0;
+            document.getElementById('endPlay').textContent = 'PERDU !'
+           }
+           document.getElementById('lifeLeft').textContent = lifes; 
+        }
+
+        
         console.log(answerWord);
         element.target.style.backgroundColor = '#D1C8E1';
         element.target.disabled = true;
@@ -67,8 +86,9 @@ function chooseLetter(element) {
 
 }
 
-document.addEventListener('click', chooseLetter);
 
+
+//Remplacer underscore par lettre cliquée
 function replaceLetters(letter, arrayPosition) {
     let arrayWordString = wordString.split('');
     for (i = 0; i < arrayPosition.length; i++) {
